c5b794cad17e3319f58e65fab69f80d3ca322663
Satabdi <satabdiraniweb@gmail.com>
2023-11-29 23:01:25 +0600

    Menu fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,13 +6,13 @@ import { MdFavorite, MdHelp } from "react-icons/md";
 import { FaWallet, FaUserFriends } from "react-icons/fa";
 
 const Navbar = () => {
-   const [nav, setNav] = useState(true);
+   const [nav, setNav] = useState(false);
 
 
     return (
         <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
             <div className="flex items-center">
-            <div onClick={setNav(!nav)} className="cursor-pointer ">
+            <div onClick={()=>setNav(!nav)} className="cursor-pointer ">
               <AiOutlineMenu size={30} />
             </div>
             <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2 ">Best 
@@ -39,8 +39,8 @@ const Navbar = () => {
             
             
             {/* side drawer */}
-            <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
-                <AiOutlineClose className="absolute right-4 top-4 cursor-ponter" size={30} />
+            <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'}>
+                <AiOutlineClose onClick={()=> setNav(!nav)} className="absolute right-4 top-4 cursor-ponter" size={30} />
                 <h2 className="text-2xl p-4">Best  <span className="font-bold">Eats</span></h2>
                 <nav>
                     <ul className="flex flex-col p-4 text-gray-800">
